ca79fba3034e07c7d25a15cd0350cd6c446c189e
Eldar Zainashev <aeoloth@yandex.ru>
2021-08-28 12:15:33 +0300

    commit

--- l01-my-app/src/index.js
+++ l01-my-app/src/index.js
@@ -1,16 +1,90 @@
-import React from 'react';
-import 'antd/dist/antd.css';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { Component, Fragment, PureComponent } from "react";
+import "antd/dist/antd.css";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const MyComponent = () => <div>
-  <h2>Локальное время { new Date().toLocaleTimeString() }</h2>
-</div>;
+// const myElement1 = <div><h2>Локальное время { new Date().toLocaleTimeString() }</h2></div>;
+// const MyComponent1 = () => myElement1;
 
-setInterval(Tick, 1000);
-function Tick() {
-ReactDOM.render(
-<MyComponent />,
-  document.getElementById('root')
-);
-}
+// setInterval(Tick, 1000);
+
+// function Tick() {
+// ReactDOM.render(
+// <MyComponent1 />,
+//   document.getElementById('root')
+// );
+// }
+
+//---------------------------------task2
+
+// const myElement1 = (
+//   <div>
+//     <h2>My element 1</h2>
+//   </div>
+// );
+// const MyComponent1 = () => myElement1;
+// const myComponent1_Element1 = <MyComponent1 />;
+
+// const myElement2 = (
+//   <div>
+//     <h2>My element 2</h2>
+//   </div>
+// );
+// function MyComponent2() {
+//   return myElement2;
+// }
+
+// const myElement3 = (
+//   <div>
+//     <h2>My element 3</h2>
+//   </div>
+// );
+
+// class MyComponent3 extends React.Component {
+//   render() {
+//     return myElement3;
+//   }
+// };
+
+// const myElement4 = (
+//   <div>
+//     <h2>My element 4</h2>
+//   </div>
+// );
+// class MyComponent4 extends PureComponent {
+//   render() {
+// return myElement4;
+//   };
+// };
+
+// ReactDOM.render(
+//   <div id="root1">
+//     <React.Fragment>
+//       <MyComponent1 />
+//       <MyComponent2 />
+//       <MyComponent3 />
+//       <MyComponent4 />
+//     </React.Fragment>
+//     <Fragment>
+//       <MyComponent1 />
+//       <MyComponent2 />
+//       <MyComponent3 />
+//       <MyComponent4 />
+//     </Fragment>
+//     <>
+//       <MyComponent1 />
+//       <MyComponent2 />
+//       <MyComponent3 />
+//     </>
+//     <div id="node1">
+//       <MyComponent1 />
+//       <MyComponent2 />
+//       <MyComponent3 />
+//     </div>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+
+//----------------------------------task 3
+
